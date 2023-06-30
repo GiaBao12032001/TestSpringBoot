44e63c5e222b640d0ddee1f134e2cea789c1d1d8
GiaBao12032001 <95518513+GiaBao12032001@users.noreply.github.com>
2023-06-30 13:17:56 +0700

    Sang update

--- src/main/java/com/example/mysqltest/MySqlTestApplication.java
+++ src/main/java/com/example/mysqltest/MySqlTestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MySqlTestApplication {//Yesman
-
+// ok
 	public static void main(String[] args) {
 		SpringApplication.run(MySqlTestApplication.class, args);
 	}
